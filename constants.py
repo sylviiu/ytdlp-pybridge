80768fafe55660e13bf0a91a2bc75e1f4ddc0a7f
Syl <hi@sylviiu.dev>
2023-07-13 12:10:42 -0500

    create constants in dist-zip too

--- constants.py
+++ constants.py
@@ -5,11 +5,14 @@ import os
 
 os.makedirs('dist/build', exist_ok=True)
 
-open('dist/build/constants.json', 'w').write(json.dumps({
+str = json.dumps({
     'Build number': os.environ['GITHUB_RUN_NUMBER'] if 'GITHUB_RUN_NUMBER' in os.environ else '-1',
     'Build arch': os.environ['RUNNER_ARCH'] if 'RUNNER_ARCH' in os.environ else 'unknown',
     'Python Version': sys.version.split(' ')[0],
     'Python Implementation': sys.implementation.name,
     'Commit': os.environ['GITHUB_SHA'] if 'GITHUB_SHA' in os.environ else 'unknown',
     'Built': int(time.time() * 1000),
-}, ensure_ascii=False, default=lambda o: '<not serializable>'))
+}, ensure_ascii=False, default=lambda o: '<not serializable>')
+
+open('dist/build/constants.json', 'w').write(str)
+open('dist/build-zip/constants.json', 'w').write(str)
